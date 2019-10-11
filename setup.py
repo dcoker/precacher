624d169ac1956f0daddae74c10fc31078de11609
Doug Coker <dcoker@gmail.com>
2019-10-11 14:02:10 -0700

    setup.py: pin install_requires and fix url.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     python_requires='>=3.6.0',
     version='0.1.0',
     packages=['precacher'],
-    install_requires=['dnspython'],
+    install_requires=['dnspython>=1.16'],
     entry_points={
         'console_scripts': [
             "precacher=precacher.__main__:main",
         ]
     },
-    url='',
+    url='https://github.com/dcoker/precacher/',
     license='https://www.apache.org/licenses/LICENSE-2.0',
     author='Doug Coker',
     author_email='dcoker@gmail.com',
